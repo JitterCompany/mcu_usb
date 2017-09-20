2da024f6fa06c782df8d10ed1f7a325f69a0574a
Ingmar Jager <ingmarjager@gmail.com>
2017-09-20 18:27:31 +0200

    First working version of library

---
+++ mcu_usb/mcu_usb.h
@@ -0,0 +1,11 @@
+#ifndef __MCU_USB_H__
+#define __MCU_USB_H__
+
+#include "src/usb_core.h"
+#include "src/usb_endpoint.h"
+#include "src/usb_request.h"
+#include "src/usb_standard_request.h"
+#include "src/usb_queue.h"
+#include "src/usb_descriptors.h"
+
+#endif
